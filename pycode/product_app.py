2abe29f4353297ecb1a344c56b4c58c7b0e8d374
godikemahesh <69629676+godikemahesh@users.noreply.github.com>
2024-08-04 13:45:01 +0530

    Update product_app.py

--- pycode/product_app.py
+++ pycode/product_app.py
@@ -3,9 +3,10 @@ import find_product
 import re
 import pandas as pd
 from gpt_data import lemitization
-# Initialize session state for text inputs
-path=r"C:\Users\mahes\Downloads\suraj logo.png"
-st.logo(path)
+import plotly.express as px
+
+#path=r"C:\Users\mahes\Downloads\suraj logo.png"
+#st.logo(path)
 st.markdown(
     """
     <style>
@@ -20,19 +21,18 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Use HTML to set the heading with the custom CSS class
-
 st.markdown("<h2 class='centered-heading'>SURAJ® OptiChoice</h2>", unsafe_allow_html=True)
 st.title("Choose Your Product")
-#st.write("<h2 style='text-align: center;color: yellow;font-size:65px;'>SURAJ® OptChoice</h2>", unsafe_allow_html=True)
+#st.write("<h2 style='text-align: center;color: yellow;font-size:65px;'>SURAJ® OptiChoice</h2>", unsafe_allow_html=True)
 st.write("Paste the Urls and get to choose best products in the amazon..\n")
 
 
 if "urls" not in st.session_state:
-    st.session_state.urls = ["",""]  # Start with one empty URL field
+    st.session_state.urls = ["", ""]  # Start with one empty URL field
 if "content" not in st.session_state:
     st.session_state.content = []
-
+if "figure" not in st.session_state:
+    st.session_state.figure=[]
 
 # Function to add a new text input
 def add_text_input():
@@ -45,37 +45,48 @@ st.sidebar.title("Enter URLs here")
 for i, url in enumerate(st.session_state.urls):
     st.session_state.urls[i] = st.sidebar.text_input(f"URL {i + 1}", url, key=f"url_{i}")
 
-# Button to add new text input
-st.sidebar.button("Add URL", on_click=add_text_input)
+
 
 def generate():
     lst=[]
     title=[]
     rate=[]
+    keys=[]
+    values=[]
     urls=[]
-    specifications=[]
     for i ,url in enumerate(st.session_state.urls):
         if st.session_state.urls[i]!="":
-            tit,rank=find_product.get_data(st.session_state.urls[i])
+            details,tit,rank=find_product.get_data(st.session_state.urls[i])
             urls.append(st.session_state.urls[i])
             pattern2 = r"(\d.\d) out of 5"
             rank=re.findall(pattern2,rank)
             title.append(tit)
-            rate.append(rank)
+            cnt=0
+            for i in rank[0]:
+                if cnt==0:
+                    rate.append(eval(str(i)))
+                    cnt+=1
             with open("specify.txt", "r+",encoding="utf-8") as ps:
                 content = ps.read()
                 ps.truncate(0)
                 data = re.sub(r"â€Ž|â|€|Ž", "", content)
                 lst.append(data)
-                spec=re.search(r"specifications:",data)
-                start=spec.end()
-                specifications.append(data[start:].split())
+                for key, value in details.items():
+                    keys.append(key)
+                    values.append(value)
+
+                dataf = pd.DataFrame({
+                    'Name': keys,
+                    'Value': values
+                })
+
+                st.session_state.content.append(f"***{tit}:***")
+                st.session_state.content.append(dataf)
+                keys.clear()
+                values.clear()
     adata=lemitization(lst)
     my_text,all_texts,sim=find_product.d_prompt(adata)
-    st.session_state.content.append(f"retrived best specifications:\n{my_text}")
-    for i in specifications:
-        st.session_state.content.append(i)
-    # Combine the two lists into a list of tuples
+    st.session_state.content.append(f"retrived best specifications:\n\n{my_text}")
     df = pd.DataFrame({
         'Percentage': sim,
         'Text': title,
@@ -92,7 +103,7 @@ def generate():
     df_sorted = df.sort_values(by='Percentage',ascending=False)
     df_sorted1 = df1.sort_values(by='rating', ascending=False)
 
-    # Extract the sorted lists
+
     sorted_percentages = df_sorted['Percentage'].tolist()
     sorted_texts = df_sorted['Text'].tolist()
     sorted_urls=df_sorted["urls"].tolist()
@@ -104,7 +115,6 @@ def generate():
 
     st.session_state.content.append("<h3 style='text-align: left;color: white;font-size:45px;'>Ranking by Specifications</h3>")
     for i in range(len(title)):
-        #st.write("[Click here to visit Streamlit](https://www.streamlit.io)")
         st.session_state.content.append(f"{i+1})[{sorted_texts[i]}]({sorted_urls[i]})")
         st.session_state.content.append("\n")
     st.session_state.content.append("<h3 style='text-align: left;color: white;font-size:45px;'>Ranking by Rating</h3>")
@@ -112,14 +122,32 @@ def generate():
         st.session_state.content.append(f"{i+1})[{sorted_texts1[i]}]({sorted_urls1[i]})")
         st.session_state.content.append("\n")
 
+    # Create a pie chart using Plotly
+    fig= px.pie(values=rate, names=title, title='rating Pie Chart')
+    fig2= px.pie(values=sim, names=title, title='specifications Pie Chart')
+    # Display the pie chart in Streamlit
+    st.session_state.figure.append(fig)
+    st.session_state.figure.append(fig2)
 
-
-st.sidebar.button("process",on_click=generate)
+with st.sidebar:
+    col1, col2 ,col3= st.columns(3)
+    with col1:
+        if st.button("Add URL", key="button1"):
+            add_text_input()
+    with col3:
+        if st.button("Process", key="button3"):
+            generate()
+    with col2:
+        if st.button("Rerun",key="button2"):
+            generate()
 for i in st.session_state.content:
     if "Ranking" in i:
         st.write(i,unsafe_allow_html=True)
     else:
         st.write(i)
-
+st.session_state.content.clear()
+for i in st.session_state.figure:
+    st.plotly_chart(i)
+st.session_state.figure.clear()
 
 
